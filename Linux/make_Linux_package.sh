79349d3078b46871404de7330e85b5831fef017a
Andreas Leibl <andreas@leibl.co.uk>
2015-03-18 15:06:22 +0000

    Fix for very old tar command in SLES 10

--- Linux/make_Linux_package.sh
+++ Linux/make_Linux_package.sh
@@ -5,6 +5,10 @@ echo "Creating package..."
 #ts=$(date +'%Y%m%d%H%M%S')
 version=$(head -n 1 VERSION)
 sed "s/VERSIONINFO/$version/" < Linux/certnanny.spec.in > Linux/certnanny.spec
-tar --transform "s/^\./certnanny-$version/" --exclude '.git' -czf $HOME/rpmbuild/SOURCES/certnanny-$version.tar.gz .
+#tar --transform "s/^\./certnanny-$version/" --exclude '.git' -czf $HOME/rpmbuild/SOURCES/certnanny-$version.tar.gz .
+mkdir certnanny-$version
+tar --exclude '.git' --exclude certnanny-$version -cf - . | (cd certnanny-$version; tar xf -)
+tar -czf $HOME/rpmbuild/SOURCES/certnanny-$version.tar.gz certnanny-$version
+rm -rf certnanny-$version
 rpmbuild -bb Linux/certnanny.spec
 
